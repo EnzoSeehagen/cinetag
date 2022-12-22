da16ac2f1f72127743772eb9b1a6907094d7b5cb
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-12-22 01:34:46 -0300

    Criando primeiro contexto

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,15 +1,17 @@
-import Banner from "components/Banner";
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import styles from './Favoritos.module.css';
 import Card from "components/Card";
-import Titulo from "components/Titulo";
 import videos from 'json/db.json';
-import styles from './Inicio.module.css'
 
-export default function Inicio() {
+
+
+export default function Favoritos() {
     return (
         <>
-            <Banner imagem="home" />
-            <Titulo>
-                <h1> Um lugar para guardar seus vídeos e filmes! </h1>
+            <Banner imagem="favoritos"/>
+            <Titulo> 
+                <h1>Caralhada toda</h1>
             </Titulo>
             <section className={styles.container}>
                 {videos.map((video) => {
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
+import Cabecalho from "components/Cabecalho"
+import Container from "components/Container"
+import Rodape from "components/Rodape"
+import Favoritos from "pages/Favoritos"
 import Inicio from "pages/Inicio"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 export default function AppRoutes() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio/>} />
-            </Routes>
+            <Cabecalho />
+                <Container >
+                    <FavoritosProvider >
+                        <Routes>
+                            <Route path="/" element={<Inicio/>} />
+                            <Route path="/favoritos" element={<Favoritos/>} />
+                        </Routes>
+                    </FavoritosProvider>
+                </Container>
+            <Rodape />
         </BrowserRouter>
     )
 };

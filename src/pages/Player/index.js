850a1e86b5226c080cd38710ecae3540b0cd4a4b
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-12-23 15:35:29 -0300

    aninhando as rotas

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -3,6 +3,7 @@ import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
 import { useParams } from 'react-router-dom';
 import videos from 'json/db.json';
+import NaoEncontrada from 'pages/NaoEncontrada';
 
 export default function Player() {
 
@@ -12,6 +13,9 @@ export default function Player() {
         return video.id === Number(parametros.id);
     })
 
+    if(!video) {
+        return <NaoEncontrada />
+    }
 
     return(
         <>

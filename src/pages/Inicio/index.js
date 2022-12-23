f19de7501132e80503371feec4e1b855a4cbad46
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-12-23 16:58:12 -0300

    Externalizando json

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,21 @@
 import Banner from "components/Banner";
 import Card from "components/Card";
 import Titulo from "components/Titulo";
-import videos from 'json/db.json';
+import { useEffect, useState } from "react";
 import styles from './Inicio.module.css'
 
 export default function Inicio() {
+
+    const [videos, setVideos] = useState({});
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/EnzoSeehagen/cinetag-api/db/videos')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideos(dados)
+        })
+    }, [])
+
     return (
         <>
             <Banner imagem="home" />
